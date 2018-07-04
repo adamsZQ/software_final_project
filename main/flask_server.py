c8dfb38c03889693f682ae2520602c2785b80158
adamszhu <adamszhu97@gmail.com>
2018-07-04 17:30:32 +0800

    update import

--- main/flask_server.py
+++ main/flask_server.py
@@ -5,15 +5,14 @@
 import os
 from flask import Flask
 import json
-import lucene
-#from lucene import VERSION, initVM, Version, WhitespaceAnalyzer, QueryParser, IndexSearcher, SimpleFSDirectory, File, getVMEnv
+from lucene import VERSION, initVM, Version, WhitespaceAnalyzer, QueryParser, IndexSearcher, SimpleFSDirectory, File, getVMEnv,IndexWriter,Field, Document
 import uuid
 
 from ltp_test import sentence_split, get_stop_words
 
 app = Flask(__name__)
 storeDir = 'qNa'
-analyzer = lucene.WhitespaceAnalyzer(lucene.Version.LUCENE_CURRENT)
+analyzer = WhitespaceAnalyzer(Version.LUCENE_CURRENT)
 stop_words = get_stop_words()
 
 
@@ -30,25 +29,25 @@ def chat_teaching(data):
 
     if not os.path.exists(storeDir):
         os.mkdir(storeDir)
-    store = lucene.SimpleFSDirectory(lucene.File(storeDir))
-    writer = lucene.IndexWriter(store, lucene.WhitespaceAnalyzer(lucene.Version.LUCENE_CURRENT),True,
-                                     lucene.IndexWriter.MaxFieldLength.LIMITED)
+    store = SimpleFSDirectory(File(storeDir))
+    writer = IndexWriter(store, WhitespaceAnalyzer(Version.LUCENE_CURRENT),True,
+                                     IndexWriter.MaxFieldLength.LIMITED)
     writer.setMaxFieldLength(1048576)
 
     # 存入lucene库
     question = question.decode('utf-8').strip()
     answer = answer.decode('utf-8').strip()
     try:
-        doc = lucene.Document()
-        doc.add(lucene.Field("id", str(uuid.uuid1()),
-                             lucene.Field.Store.YES,
-                             lucene.Field.Index.NOT_ANALYZED))
-        doc.add(lucene.Field("question", question,
-                             lucene.Field.Store.YES,
-                             lucene.Field.Index.ANALYZED))
-        doc.add(lucene.Field("answer", answer,
-                             lucene.Field.Store.YES,
-                             lucene.Field.Index.NOT_ANALYZED))
+        doc = Document()
+        doc.add(Field("id", str(uuid.uuid1()),
+                             Field.Store.YES,
+                             Field.Index.NOT_ANALYZED))
+        doc.add(Field("question", question,
+                             Field.Store.YES,
+                             Field.Index.ANALYZED))
+        doc.add(Field("answer", answer,
+                             Field.Store.YES,
+                             Field.Index.NOT_ANALYZED))
         writer.addDocument(doc)
     except Exception, e:
         print e
@@ -60,11 +59,11 @@ def fuzzy_matching(question):
     question = sentence_split(question)
 
     # 构建查找器
-    directory = lucene.SimpleFSDirectory(lucene.File("qNa"))
-    searcher = lucene.IndexSearcher(directory, True)
+    directory = SimpleFSDirectory(File("qNa"))
+    searcher = IndexSearcher(directory, True)
 
     # 开始查询
-    query = lucene.QueryParser(lucene.Version.LUCENE_CURRENT, "question",
+    query = QueryParser(Version.LUCENE_CURRENT, "question",
                         analyzer).parse(question)
     scoreDocs = searcher.search(query, 1).scoreDocs
     if len(scoreDocs) < 1:

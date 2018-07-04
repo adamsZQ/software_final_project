050ff52b0be6a219fab7db1835ce0c206c333790
adamszhu <adamszhu97@gmail.com>
2018-07-04 16:57:34 +0800

    add interface method of ltp

--- main/ltp_test.py
+++ main/ltp_test.py
@@ -56,20 +56,26 @@ def LTP_ne(sentence):
 
     return seg_list, pos_list, ner_list
 
-t0 = time.time()
-print "LTP预处理开始"
-time_LTP = time.time()
-content = "我爱你我亲爱的母亲"
-seg_list, pos_list, arc_list = LTP_dp(content)
-seg_list, pos_list, ner_list = LTP_ne(content)
-seg = ' '.join(seg_list)
-pos = ' '.join(pos_list)
-arc = ' '.join(arc_list)
-ner = ' '.join(ner_list)
-print "LTP预处理用时:" + str(time.time() - time_LTP)
-print "——————————"
-print '*seg:', seg
-print '*pos:', pos
-print '*arc:', arc
-print '*ner:', ner
-print "——————————"
+# 传入一个句子，返回一个带空格的字符串
+def sentence_split(sentence):
+    t0 = time.time()
+    print "LTP预处理开始"
+    time_LTP = time.time()
+    content = sentence
+    seg_list, pos_list, arc_list = LTP_dp(content)
+    seg_list, pos_list, ner_list = LTP_ne(content)
+    seg = ' '.join(seg_list)
+    pos = ' '.join(pos_list)
+    arc = ' '.join(arc_list)
+    ner = ' '.join(ner_list)
+    print "LTP预处理用时:" + str(time.time() - time_LTP)
+    print "——————————"
+    print '*seg:', seg
+    print '*pos:', pos
+    print '*arc:', arc
+    print '*ner:', ner
+    print "——————————"
+
+if __name__ == '__main__':
+    sentence_split("我觉得你说的，可能存在问题，你觉得呢？")
+
